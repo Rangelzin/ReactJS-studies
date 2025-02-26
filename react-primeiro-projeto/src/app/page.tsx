7eb5ae8a20d480c9dc3b7c5411445b4217882bc5
Rangelzin <victorrangeltasse85@hotmai.com>
2025-02-25 22:23:03 -0300

    Primeiros projetos!

---
+++ react-primeiro-projeto/src/app/page.tsx
@@ -0,0 +1,10 @@
+const Page = () => {
+  return (
+    <div>
+      <h1 className="bg-white text-black font-bold"> ola mundo!</h1>
+      <p>oi pessoa!</p>
+    </div>
+  )
+}
+
+export default Page;
