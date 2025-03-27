32564086171493a2f3dfb31ae207a3366a62c191
Rangelzin <victorrangeltasse85@hotmai.com>
2025-03-27 15:21:37 -0300

    Continuação do projeto

--- ByVite/react-primeiro-projeto/src/App.jsx
+++ ByVite/react-primeiro-projeto/src/App.jsx
@@ -3,7 +3,7 @@ import Taks from "./components/Tasks";
 
 function App() {
   return (
-    <div>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <h1 className="text-red-500 text-2xl">Gerenciador de Tarefas</h1>
       <Taks />
       <AddTaks />
